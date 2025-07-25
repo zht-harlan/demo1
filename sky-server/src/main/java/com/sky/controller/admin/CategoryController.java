d0d56ed035a625ddb43208beed9a151bacf011b6
harland <15894877+harlandZheng@user.noreply.gitee.com>
2025-07-25 11:07:13 +0800

    分类功能

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,4 +1,33 @@
 package com.sky.controller.admin;
 
+
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/admin/category")
+@Api(tags = "分类管理接口")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @ApiOperation(value = "分页查询分类列表")
+    @GetMapping("/page")
+    public Result pageguery(CategoryPageQueryDTO categoryPageQueryDTO) {
+        log.info("分页查询分类列表: {}", categoryPageQueryDTO);
+        PageResult pageResult =categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
 }
