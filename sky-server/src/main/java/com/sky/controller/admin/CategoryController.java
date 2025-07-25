144cc01a1f9729638e773f99c8fd53fc1d6d6ddc
harland <15894877+harlandZheng@user.noreply.gitee.com>
2025-07-25 13:58:45 +0800

    分类功能

--- sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 
+import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
@@ -9,9 +10,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -22,6 +21,12 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    /**
+     * 分页查询分类列表
+     *
+     * @param categoryPageQueryDTO 分页查询条件
+     * @return 分页结果
+     */
     @ApiOperation(value = "分页查询分类列表")
     @GetMapping("/page")
     public Result pageguery(CategoryPageQueryDTO categoryPageQueryDTO) {
@@ -30,4 +35,33 @@ public class CategoryController {
         return Result.success(pageResult);
     }
 
+    @PutMapping
+    @ApiOperation(value = "更新分类信息")
+    public Result updateCategory(@RequestBody CategoryDTO categoryDTO) {
+        log.info("更新分类信息: {}", categoryDTO);
+        categoryService.updateCategory(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 根据类型查询所有分类列表
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation(value = "根据类型查询所有分类列表")
+    public Result list(@RequestParam("type") Integer type) {
+        log.info("根据类型查询分类列表: type={}", type);
+        return Result.success(categoryService.listByType(type));
+    }
+
+
+    @DeleteMapping("/{id}")
+    @ApiOperation(value = "删除分类")
+    public Result deleteCategory(@RequestParam("id") Long id) {
+        log.info("删除分类: id={}", id);
+        // 这里可以添加删除逻辑
+        categoryService.deleteCategory(id);
+        return Result.success();
+    }
 }
